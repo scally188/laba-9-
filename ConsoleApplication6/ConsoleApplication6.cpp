e962db736e5c3a39faceb1aaf4327b19383bbd70
scally188 <registersc2@gmail.com>
2023-12-05 16:25:38 +0200

    nomber 1

---
+++ ConsoleApplication6/ConsoleApplication6.cpp
@@ -0,0 +1,53 @@
+﻿
+
+#include <iostream> 
+#define _USE_MATH_DEFINES
+#include <math.h>
+using namespace std;
+#include "windows.h";
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	int n; 
+	cout << "enter n";
+	cin >> n;
+	switch (n) {
+	case 1:
+		cout << "Меркурий";
+		break;
+
+	case 2:
+		cout << "Венера";
+		break;
+
+	case 3:
+		cout << "Земля";
+		break;
+	case 4: 
+		cout << "Марс";
+		break;
+
+	case 5:
+		cout << "Юпитер";
+		break;
+	case 6:
+		cout << "Сатурн";
+		break;
+	case 7: 
+		cout << "Уран";
+		break;
+	case 8:
+		cout << "Нептун";
+		break;
+	default:
+		cout << "планета не існує";
+		break;
+
+
+	}
+	
+	
+}
+
